4ce06be7e79db8968cf53c71637629f88def65c6
Abner Xoip <abnerxoip@gmail.com>
2025-11-25 13:33:16 -0700

    cambios

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,15 @@
 let rutaBase = '';
 
-if (window.location.pathname.includes('/html/')) {
-  rutaBase = '../css/navbar.css'; // si estás en una subcarpeta
+// se comprueba si esta en una subcarpeta como /login/ o /html/
+if (window.location.pathname.includes('/login/') || window.location.pathname.includes('/html/')) {
+  rutaBase = '../'; // Si es así, debemos subir un nivel
 }
 
+// En index.html, rutaBase será '' -> fetch('navbar/navbar.html')
+// En login.html, rutaBase será '../' -> fetch('../navbar/navbar.html')
 fetch(rutaBase + 'navbar/navbar.html')
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById('navbar-container').innerHTML = data;
-  })
-  .catch(err => console.error('Error al cargar el navbar:', err));
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('navbar-container').innerHTML = data;
+  })
+  .catch(err => console.error('Error al cargar el navbar:', err));
